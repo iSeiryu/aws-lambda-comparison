35da51211fa29327645dd25afe80167eff07c41e
Sergey Ivanov <sergey.ivanov@willowtreeapps.com>
2022-07-04 10:40:59 -0400

    Initial setup with lambda.AspNetCoreWebApi project.

---
+++ dotnet-aspnet/src/BrightlineBff/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrightlineBff.Controllers;
+
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    // POST api/values
+    [HttpPost]
+    public Book Post([FromBody]Book book) {
+        book.title += "modified";
+        book.title = book.title.ToUpper();
+        book.published = book.published.AddDays(1);
+        book.completed = !book.completed;
+
+        return book;
+    }
+
+    public class Book {
+        public int id {get;set;}
+        public DateTime published {get;set;}
+        public string title {get;set;}
+        public bool completed {get;set;}
+    }
+}
